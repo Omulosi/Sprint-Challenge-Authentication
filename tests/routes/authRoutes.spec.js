87c1e484d8a45a56f060d2b9fa58acad0f30cc61
Omulosi <mulongojohnpaul@gmail.com>
2019-11-15 15:05:54 +0300

    Update tests

--- tests/routes/authRoutes.spec.js
+++ tests/routes/authRoutes.spec.js
@@ -1,15 +1,27 @@
-const server = require('./api/server.js');
+const server = require('../../api/server.js');
 const request = require('supertest');
 
+describe('Authentication routes', () => {
 
-describe('Login route', () => {
+  describe('POST /api/auth/login', () => {
 
-  it('rejects invalid user data', () => {
-    return request(server).post('/api/auth/login')
-      .expect(400)
+    it('rejects invalid user data', () => {
+      return request(server).post('/api/auth/login')
+        .send({"username": "papa"})
+        .expect(400)
+    })
+
+    it('accepts a valid user', () => {
+      return request(server).post('/api/auth/login')
+        .send({username: "hellen", password: "1234abcd"})
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.have.a.property('token')
+        })
+
+    })
   })
-  it('accepts a valid user', () => {
-    return request(server).post('/api/auth/login')
-      .expect(400)
-  })
+
+
 })
+
--- tests/routes/jokesRoutes.spec.js
+++ tests/routes/jokesRoutes.spec.js
@@ -1,12 +1,12 @@
-const server = require('./api/server.js');
+const server = require('../../api/server.js');
 const request = require('supertest');
 
 
 describe('GET /api/jokes', () => {
 
-  it('returns 200 OK', () => {
-    return request(server).get('/')
-      .expect(200)
+  it('returns 400 when no token passed', () => {
+    return request(server).get('/api/jokes')
+      .expect(400)
       .expect('Content-Type', /json/)
   })
 })
